8c93311f155a3ba48456b66cbd2ce33308cf1e3c
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-01-04 20:36:12 +0300

    edited link list

--- index.php
+++ index.php
@@ -101,32 +101,33 @@
           <div class="row">
             <div class="col-6">
             <ul class="list-group">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+
 </ul>
             </div>
             <div class="col-6">
             <ul class="list-group">
-  <li class="list-group-item">An item</li>
-  <li class="list-group-item">A second item</li>
-  <li class="list-group-item">A third item</li>
-  <li class="list-group-item">A fourth item</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
-  <li class="list-group-item">And a fifth one</li>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
+            <a href="#" class="list-group-item list-group-item-action">A second link item</a>
 </ul>
             </div>
           </div>
