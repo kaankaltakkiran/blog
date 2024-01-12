9be7fcbf130927ddf65ea8d8faddaa84b5082fad
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-01-12 11:41:16 +0300

    fixed project folder

--- admin.login.php
+++ admin.login.php
@@ -143,7 +143,7 @@ if (!empty($errors)) {
 </div>
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="hideShow.js"></script>
-    <script src="autoCloseAlert.js"></script>
+    <script src="./public/js/hideShow.js"></script>
+    <script src="./public/js/autoCloseAlert.js"></script>
   </body>
 </html>
--- blog.update.php
+++ blog.update.php
@@ -183,6 +183,6 @@ foreach ($categories as $category) {
 
 </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="./autoCloseAlert.js"></script>
+    <script src="./public/js/autoCloseAlert.js"></script>
   </body>
 </html>
