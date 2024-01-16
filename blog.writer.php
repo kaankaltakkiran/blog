d607be4fe3ced66982e07ffca6061217f0bbc3fb
kaankaltakkiran <durdu.kaan.kaltakkiran@gmail.com>
2024-01-16 16:34:11 +0300

    added writer blog delete with bootsrap modal

--- blog.writer.php
+++ blog.writer.php
@@ -18,16 +18,35 @@ $SORGU->bindParam(':writerid', $id);
 $SORGU->execute();
 $blogs = $SORGU->fetchAll(PDO::FETCH_ASSOC);
 /* echo '<pre>';
-print_r($blogs); */
+print_r($blogs);
+die();
+ */
+//!Eğer blog silme isteği gelirse silme işlemini yap ve tekrardan yazar sayfasına yönlendir.
+if (isset($_GET['remove'])) {
+    require 'db.php';
+    $remove_id = $_GET['remove'];
+    $id = $_SESSION['id'];
 
+    $sql = "DELETE FROM blogs WHERE blogid = :remove";
+    $SORGU = $DB->prepare($sql);
+
+    $SORGU->bindParam(':remove', $remove_id);
+
+    $SORGU->execute();
+    echo "<script>
+    alert('The blog has been deleted. You are redirected to the Writer page...!');
+    window.location.href = 'blog.writer.php?writerid={$id}';
+    </script>";
+}
 //! Kontrol: Eğer kayıt yoksa hata sayfasına yönlendir
 if (empty($blogs)) {
     echo "<script>
-  alert('Theree is no record in the database. You are redirected to the home page...!');
-  window.location.href = 'index.php';
+alert('Theree is no record in the database. You are redirected to the home page...!');
+window.location.href = 'index.php';
 </script>";
 }
 ?>
+
   <!doctype html>
   <html lang="en">
     <head>
@@ -35,6 +54,7 @@ if (empty($blogs)) {
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title><?php echo $blogs[0]['username'] ?>'s Blog</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     </head>
     <body>
     <!--  Navbar için navbar.php dosyasını include ediyoruz. -->
@@ -94,6 +114,29 @@ if ($_SESSION['id'] == $blogs[0]['userid']) {
     <?php
 //!publish olsun denen ve tarihleri uygun olan blogları getir
 foreach ($blogs as $blog) {
+    //!Chatgpt çözümü ile modala farklı id vermek
+    $modalId = "exampleModal{$blog['blogid']}";
+    //!Silmek için oluşturlan bootstrap modal yapısı
+    echo "
+     <!-- Modal -->
+     <div class='modal fade' id='{$modalId}' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+       <div class='modal-dialog'>
+         <div class='modal-content'>
+           <div class='modal-header'>
+             <h1 class='modal-title fs-5' id='exampleModalLabel'>Delete {$blog['title']}? </h1>
+             <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+           </div>
+           <div class='modal-body'>
+           {$blog['username']}, Are you sure you want to delete the blog?
+           </div>
+           <div class='modal-footer'>
+             <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Close</button>
+             <a href='blog.writer.php?remove={$blog['blogid']}' class='btn btn-danger'>Delete </a>
+           </div>
+         </div>
+       </div>
+     </div>
+     ";
     if ($blog['ispublish'] == 1 && strtotime($blog['startdate']) <= time() && strtotime($blog['lastdate']) >= time()) {
         echo "
         <div class='row'>
@@ -109,12 +152,16 @@ foreach ($blogs as $blog) {
                   </p>
                   <div class='d-flex justify-content-between align-items-center'>
                       <div class='btn-group'>
-                          <a class='btn btn-outline-success' href='blog.show.php?blogid={$blog["blogid"]}'>Read More...</a>
+                          <a class='btn btn-outline-info' href='blog.show.php?blogid={$blog["blogid"]}'>Read More...</a>
                       </div>";
 
         if ($_SESSION['id'] == $blog['userid']) {
-            // Display the "Update Blog" button if the user is the owner of the blog
-            echo "<a class='btn btn-danger' href='blog.update.php?blogid={$blog["blogid"]}'>Update Blog</a>";
+            echo "<a class='btn btn-outline-success' href='blog.update.php?blogid={$blog["blogid"]}'>Update Blog</a>";
+            echo "<a href='blog.writer.php?remove={$blog['blogid']}' class='btn btn-outline-danger' data-bs-toggle='modal' data-bs-target='#{$modalId}'>
+           Delete Blog
+           <i class='bi bi-trash'></i>
+          </a>
+          ";
         }
 
         echo "<div style='float: right;'><span style='color: DimGray;'>Writer:</span>
@@ -142,9 +189,13 @@ foreach ($blogs as $blog) {
                   </p>
                   <div class='d-flex justify-content-between align-items-center'>
                       <div class='btn-group'>
-                          <a class='btn btn-outline-success' href='blog.show.php?blogid={$blog["blogid"]}'>Read More...</a>
+                          <a class='btn btn-outline-info' href='blog.show.php?blogid={$blog["blogid"]}'>Read More...</a>
                       </div>
-                      <a class='btn btn-danger' href='blog.update.php?blogid={$blog["blogid"]}'>Update Blog</a>
+                      <a class='btn btn-outline-success' href='blog.update.php?blogid={$blog["blogid"]}'>Update Blog</a>
+                      <a href='blog.writer.php?remove={$blog["blogid"]}' class='btn btn-outline-danger' data-bs-toggle='modal' data-bs-target='#{$modalId}'>
+                     Delete Blog
+                     <i class='bi bi-trash'></i>
+                    </a>
                       <div style='float: right;'><span style='color: DimGray;'>Writer:</span>
                           <a href='blog.writer.php?writerid={$blog["userid"]}' class='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover'>{$blog['username']}</a>
                           <br>
@@ -158,6 +209,7 @@ foreach ($blogs as $blog) {
     }
 }
 
+;
 ?>
 
     </div>
